e33d44411283d8981e6d5535eb1863abf06e0c18
unzurna <unzurna@students.amikom.ac.id>
2025-07-11 14:28:16 +0700

    responsi

---
+++ backend/data.js
@@ -0,0 +1,27 @@
+// backend/data.js
+const educationHistory = [ 
+    { id: 1,
+     period: '2023 - Sekarang',
+     institution: 'Universitas AMIKOM Yogyakarta',
+     major: 'S1 - Teknik Informatika' 
+    },
+    { id: 2,
+     period: '2018 - 2021',
+     institution: 'SMK INFORMATIKA',
+     major: 'Teknik Komputer Jaringan' 
+    }
+];
+const skills = [ 
+    { name: 'Vue.js', level: 'Pemula' }, { name: 'JavaScript', level: 'Pemula' },
+    { name: 'Tailwind CSS', level: 'Pemula' }, { name: 'Node.js', level: 'Pemula' },
+    { name: 'Express.js', level: 'Pemula' }, { name: 'PostgreSQL', level: 'Pemula' },
+    { name: 'Git & GitHub', level: 'Pemula' }, { name: 'HTML5 & CSS3', level: 'Pemula' }
+];
+const projects = [ 
+    { title: 'Notifikasi Damage Game', image: '/src/assets/mendaki.jpeg',
+    description: 'Sistem Notifikasi Damage Game Sederhana', tech: ['C#'
+], link: '#' },
+    { title: 'Player MOBA', image:'/src/assets/ml.jpeg' ,
+    description: 'Push Ranks', tech: ['Protheus 8', 'CVAVR'], link: '#' }
+];
+module.exports = { educationHistory, skills, projects };
---
+++ frontend/src/components/Hero.vue
@@ -0,0 +1,29 @@
+<template>
+  <section id="profil" class="container mx-auto px-6 py-20 flex flexcol md:flex-row items-center">
+    <div class="md:w-1/2 mb-10 md:mb-0">
+      <h1
+        class="text-5xl font-bold text-gray-800 mb-4 animate-slidein-left"
+        style="animation-delay: 200ms"
+      >
+        Halo, Saya <span class="text-blue-600">Aditya Uzman Chandra Nugraha</span>
+      </h1>
+      <p class="text-xl text-gray-600 mb-8 animate-slide-in-left" style="animation-delay: 400ms">
+        Mahasiswa Teknik Informatika yang sangat senang dalam pengembangan web.
+      </p>
+      <a
+        href="wa.me/6289515004836"
+        class="bg-blue-600 text-white font-bold py-3 px-6 rounded-lg hover:bg-blue-700 transition-colors duration-300 inline-block animate-slide-in-left"
+        style="animation-delay: 600ms"
+      >
+        Hubungi Saya
+      </a>
+    </div>
+    <div class="md:w-1/2 flex justify-center animate-slide-in-right">
+      <img
+        src="C:\Users\LENOVO\Documents\portomail\interactiv-cv\frontend\src\assets\mail.jpeg"
+        alt="Foto Profil"
+        class="w-80 h-80 rounded-full object-cover shadow-lg"
+      />
+    </div>
+  </section>
+</template>
